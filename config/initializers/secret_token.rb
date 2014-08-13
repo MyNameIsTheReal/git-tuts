4bdafae3f125e0ef32cd65f9f3f1555ae39a3d43
MyNameIsTheReal <francisdaryl.guerrero@gmail.com>
2014-08-13 16:41:39 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitTuts::Application.config.secret_key_base = '6796447779ac5b7492afc4deaab311c02732612e260997bfa931d8be5e49e75b178b5807077ca5b54a1b92b57632d1fba85ef97502edda20c2598667168415a0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GitTuts::Application.config.session_store :cookie_store, key: '_git-tuts_session'
